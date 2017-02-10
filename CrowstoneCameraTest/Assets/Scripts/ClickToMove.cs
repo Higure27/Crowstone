b6049f4d18c3f6d5d63bf5477660cdeeef81db15
Bradleycdawn <Bradleycdawn@yahoo.com>
2017-02-09 18:08:17 -0700

    Added Camera Test
    Added Camera Test Project to the Github

---
+++ CrowstoneCameraTest/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+
+    NavMeshAgent navAgent;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+    public int rotationSpeed = 500;
+
+    private Quaternion permRotation;
+
+    private bool isRotating;
+    private Vector3 direction;
+    private Quaternion lookRotation;
+    private RaycastHit hit;
+
+
+    IEnumerator RotateAgent(Quaternion currentRotation, Quaternion targetRotation) {
+
+        isRotating = true; 
+        while(currentRotation != targetRotation) {
+            transform.rotation = Quaternion.RotateTowards(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
+            currentRotation = transform.rotation;
+            yield return 1;
+        }
+        isRotating = false;
+        navAgent.SetDestination(hit.point);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        permRotation = gameObject.transform.rotation;
+        navAgent = GetComponent<NavMeshAgent>();
+        isRotating = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (navAgent.remainingDistance == 0) {
+            navAgent.ResetPath();
+        }
+
+        this.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f, 0, 0);
+        this.transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * 10.0f);
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        gameObject.transform.rotation = permRotation;
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit, 100)) {
+                if (navAgent.remainingDistance > 0)
+                    navAgent.destination = gameObject.transform.localPosition;
+                direction = (hit.point - transform.position).normalized;
+                lookRotation = Quaternion.LookRotation(direction);
+
+                //StartCoroutine(RotateAgent(transform.rotation, lookRotation));
+                navAgent.SetDestination(hit.point);
+            }
+        }
+	}
+}
