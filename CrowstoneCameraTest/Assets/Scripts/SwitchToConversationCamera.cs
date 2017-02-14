a4788de1dd4a673489e0340b8f32da02e6a15a14
Bradleycdawn <Bradleycdawn@yahoo.com>
2017-02-14 01:23:48 -0700

    Prototype camera switch for conversations

---
+++ CrowstoneCameraTest/Assets/Scripts/SwitchToConversationCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToConversationCamera : MonoBehaviour {
+
+    public Camera conversationCamera;
+    public Camera mainCamera;
+    private bool switchCamera;
+
+
+	// Use this for initialization
+	void Start () {
+        switchCamera = false;
+        mainCamera.enabled = true;
+        conversationCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (switchCamera) {
+            mainCamera.enabled = !mainCamera.enabled;
+            conversationCamera.enabled = !conversationCamera.enabled;
+            switchCamera = false;
+        }
+	}
+
+    private void OnMouseDown() {
+        switchCamera = true;
+    }
+}
