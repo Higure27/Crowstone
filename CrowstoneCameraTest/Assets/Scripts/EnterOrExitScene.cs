b51bb76b6df200b9bf94d2f8a29f7f5338c20b2b
Bradleycdawn <Bradleycdawn@yahoo.com>
2017-02-16 17:59:41 -0700

    UI for mouse hovering and other tweaks

---
+++ CrowstoneCameraTest/Assets/Scripts/EnterOrExitScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnterOrExitScene : MonoBehaviour {
+
+    public string SceneName;
+    public GameObject player;
+    public float x, y, z;
+    private NavMeshAgent navAgent;
+    private Vector3 playerDest;
+    private bool headingToDoor;
+
+    private void Start() {
+        playerDest = new Vector3(x, y, z);
+        navAgent = player.GetComponent<NavMeshAgent>();
+        headingToDoor = false;
+    }
+
+    private void Update() {
+        if (headingToDoor && navAgent.remainingDistance == 0) {
+            headingToDoor = false;
+            navAgent.ResetPath();
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+
+    private void OnMouseDown() {
+        navAgent.SetDestination(playerDest);
+        headingToDoor = true;
+    }
+}
