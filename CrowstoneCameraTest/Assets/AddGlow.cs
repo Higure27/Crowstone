dc3a97066f8bd566a5a6e2fd240c29b4a8f0ffd8
executioNstyleZ <jared.kc.shaw@gmail.com>
2017-02-23 14:46:49 -0700

    added script that identifies when player is looking at the object, also calculates the distance between the object and player to see if it is acceptable to glow

--- CrowstoneCameraTest/Assets/AddGlow.cs
+++ CrowstoneCameraTest/Assets/AddGlow.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class AddGlow : MonoBehaviour {
 
+    Transform playerTransform;
+    public float maxDistanceToGlow = 3.5f;
+
 	// Use this for initialization
 	void Start () {
-		
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
     void OnMouseOver()
     {
-        Debug.Log("hovering over: " + gameObject.name);
+        if(DistanceBetweenThisAndPlayer() <= maxDistanceToGlow)
+        {
+            Debug.Log("acceptable distance to glow");
+        }
     }
 
     void OnMouseExit()
     {
-        Debug.Log("no longer hovering over: " + gameObject.name);
+        
+    }
+
+    private float DistanceBetweenThisAndPlayer()
+    {
+        float result = 0.0f;
+
+        float x1 = transform.position.x;
+        float y1 = transform.position.y;
+        float z1 = transform.position.z;
+
+        float x2 = playerTransform.position.x;
+        float y2 = playerTransform.position.y;
+        float z2 = playerTransform.position.z;
+
+        //distance formula
+        result = Mathf.Sqrt(Mathf.Pow((x1 - x2), 2) + Mathf.Pow((y1 - y2), 2) + Mathf.Pow((z1 - z2), 2));
+
+        return result;
     }
 }
