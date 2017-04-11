8f9a53a7d50777ca493f7260bc5f81893be9aac6
Bradleycdawn <Bradleycdawn@yahoo.com>
2017-04-11 08:13:41 -0600

    Linked school and bank inside scenes to town facades

--- CrowstoneCameraTest/Assets/Scripts/GameManagement/SetStartLocation.cs
+++ CrowstoneCameraTest/Assets/Scripts/GameManagement/SetStartLocation.cs
@@ -9,12 +9,20 @@ public class SetStartLocation : MonoBehaviour {
     void Start() {
         if (GameManager.gameManager.getPreviousLocation().Equals("Saloon")) {
             gameObject.transform.position = new Vector3(-18.6f, 0f, -20f);
-            gameObject.transform.Rotate(0, -140, 0);
+            gameObject.transform.Rotate(0, -50, 0);
         }
         else if (GameManager.gameManager.getPreviousLocation().Equals("Jail")) {
             gameObject.transform.position = new Vector3(-15.71f, 0f, -6.85f);
-            gameObject.transform.Rotate(0, 90, 0);
+            gameObject.transform.Rotate(0, 180, 0);
         }
-        
+        else if (GameManager.gameManager.getPreviousLocation().Equals("BankInterior")) {
+            gameObject.transform.position = new Vector3(-6.553f, 0f, -8.411f);
+            gameObject.transform.Rotate(0, 180, 0);
+        }
+        else if (GameManager.gameManager.getPreviousLocation().Equals("SchoolInterior")) {
+            gameObject.transform.position = new Vector3(.09f, 0f, -8.23f);
+            gameObject.transform.Rotate(0, 180, 0);
+        }
+
     }
 }
